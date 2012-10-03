c38548b1e6576ddecb7d0728eb62e6f3acbd3f5b
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-03 05:38:07 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "cage", description: "Rack-inspired HTTP stack handling library for Elixir",
+                 version: :head, keywords: ["http","stack","cowboy","adapter","rack"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/cage"]])
+    
