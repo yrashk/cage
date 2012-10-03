ca97233c50f9bec13fe86e25d219a20d57b7445b
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-03 14:27:53 -0700

    Minor Cage.Stack refactoring

--- lib/stack.ex
+++ lib/stack.ex
@@ -3,13 +3,14 @@ defmodule Cage.Stack do
     quote do
       Module.register_attribute __MODULE__, :__stack__
 
-      def run(conn) do
-        state = Cage.State.new
-        units = lc {:__stack__, [unit]} inlist __MODULE__.__info__(:attributes), do: unit
-        Enum.reduce units, {conn, state}, fn({unit, opts}, {conn, state}) -> apply(unit, :call, [conn, state, opts]) end
-      end
-
+      def run(conn), do: Cage.Stack.run(conn, __MODULE__)
     end
+  end
+
+  def run(conn, module) do
+    state = Cage.State.new
+    units = lc {:__stack__, [unit]} inlist module.__info__(:attributes), do: unit
+    Enum.reduce units, {conn, state}, fn({unit, opts}, {conn, state}) -> apply(unit, :call, [conn, state, opts]) end
   end
 
 end
